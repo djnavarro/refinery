d3c9d0e3bc896c9ea9b55604a606bf4534d4ea23
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-06 00:20:38 +1000

    adds record_lockfile

--- R/appendices.R
+++ R/appendices.R
@@ -1,14 +1,32 @@
 
 
-#' Writes a time stamp to the R markdown output
+#' Records a time stamp to the R markdown output
 #'
 #' @param tzone Character vector specifying the time zone
 #'
 #' @return A "shiny.tag" object
 #' @export
-timestamp <- function(tzone = Sys.timezone()) {
+record_timestamp <- function(tzone = Sys.timezone()) {
   time <- lubridate::now(tzone = tzone)
   stamp <- as.character(time, tz = tzone, usetz = TRUE)
   return(htmltools::p(stamp))
 }
 
+#' Records a link to the lock file
+#'
+#' @param dir The folder in which the article is located
+#' @param collection The collection the article belongs to (default = "_posts")
+#' @param repo_spec Repository specification, formatted as "user/repo_name"
+#' @param branch Name of the branch to look at
+#' @param host Host URL
+#'
+#' @return A "shiny.tag" object
+#' @export
+record_lockfile <- function(repo_spec, dir, collection = "_posts",
+                            branch = "master", host = "https://github.com") {
+  path <- paste(
+    host, repo_spec, "tree", branch, "_refinery", dir,
+    collection, "renv.lock", sep = "/"
+  )
+  return(htmltools::a(href = path, "lockfile"))
+}
