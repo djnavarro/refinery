63ddfb48c8f232666b941125090ddfe5c8a9b3d2
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-06 00:33:49 +1000

    adds record_source

--- R/appendices.R
+++ R/appendices.R
@@ -9,7 +9,7 @@
 record_timestamp <- function(tzone = Sys.timezone()) {
   time <- lubridate::now(tzone = tzone)
   stamp <- as.character(time, tz = tzone, usetz = TRUE)
-  return(htmltools::p(stamp))
+  return(htmltools::span(stamp))
 }
 
 #' Records a link to the lock file
@@ -19,14 +19,37 @@ record_timestamp <- function(tzone = Sys.timezone()) {
 #' @param repo_spec Repository specification, formatted as "user/repo_name"
 #' @param branch Name of the branch to look at
 #' @param host Host URL
+#' @param text Text to display in the link
 #'
 #' @return A "shiny.tag" object
 #' @export
 record_lockfile <- function(repo_spec, dir, collection = "_posts",
-                            branch = "master", host = "https://github.com") {
+                            branch = "master", host = "https://github.com",
+                            text = "R environment") {
   path <- paste(
-    host, repo_spec, "tree", branch, "_refinery", dir,
-    collection, "renv.lock", sep = "/"
+    host, repo_spec, "tree", branch, "_refinery", collection, dir,
+    "renv.lock", sep = "/"
   )
-  return(htmltools::a(href = path, "lockfile"))
+  return(htmltools::a(href = path, text))
+}
+
+
+#' Records a link to the source code
+#'
+#' @param dir The folder in which the article is located
+#' @param collection The collection the article belongs to (default = "_posts")
+#' @param repo_spec Repository specification, formatted as "user/repo_name"
+#' @param branch Name of the branch to look at
+#' @param host Host URL
+#' @param text Text to display in the link
+#'
+#' @return A "shiny.tag" object
+#' @export
+record_source <- function(repo_spec, dir, collection = "_posts",
+                            branch = "master", host = "https://github.com",
+                            text = "R environment") {
+  path <- paste(
+    host, repo_spec, "tree", branch, collection, dir, sep = "/"
+  )
+  return(htmltools::a(href = path, text))
 }
