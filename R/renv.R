90bbfa2c8c6e02bbb68ee1808a96c29aa3bf37bf
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-05 19:48:35 +1000

    adds renv helpers

--- R/renv.R
+++ R/renv.R
@@ -1,13 +1,13 @@
 
 
-#' Locate the R environment for a distill article
+#' Locate the renv project environment for a distill article
 #'
 #' @param dir The folder in which the article is located
 #' @param collection The collection the article belongs to (default = "_posts")
 #'
 #' @return Character string with the path
 #' @export
-renv_locate <- function(dir, collection = "_posts") {
+renv_path <- function(dir, collection = "_posts") {
   fs::path(
     rprojroot::find_root("_site.yml"),
     "_refinery",
@@ -16,13 +16,67 @@ renv_locate <- function(dir, collection = "_posts") {
   )
 }
 
-#' Activate the R environment associated with a distll article
+post_path <- function(dir, collection = "_posts") {
+  fs::path(
+    rprojroot::find_root("_site.yml"),
+    collection,
+    dir
+  )
+}
+
+#' Locate the renv lockfile associated with a distill article
+#'
+#' @param dir The folder in which the article is located
+#' @param collection The collection the article belongs to (default = "_posts")
+#'
+#' @return Character string with the path
+#' @export
+renv_lockfile <- function(dir, collection = "_posts") {
+  fs::path(renv_path(dir, collection), "renv.lock")
+}
+
+#' Use the R environment associated with a distill article
 #'
 #' @param dir The folder in which the article is located
 #' @param collection The collection the article belongs to (default = "_posts")
 #'
 #' @return Invisibly returns the renv project directory
 #' @export
-renv_activate <- function(dir, collection = "_posts") {
-  renv::activate(project = renv_locate(dir, collection))
+renv_use <- function(dir, collection = "_posts") {
+  renv::uses(lockfile = renv_lockfile(dir, collection))
 }
+
+
+
+#' Create a snapshot of the R environment for the post
+#'
+#' @param dir The folder in which the article is located
+#' @param collection The collection the article belongs to (default = "_posts")
+#' @param ... Arguments to be passed to renv::snapshot()
+#'
+#' @return ???
+#' @export
+renv_snapshot <- function(dir, collection = "_posts", ...) {
+  renv::snapshot(
+    project = post_path(dir, collection),
+    lockfile = renv_lockfile(dir, collection),
+    ...
+  )
+}
+
+#' Restore the renv project
+#'
+#' @param dir The folder in which the article is located
+#' @param collection The collection the article belongs to (default = "_posts")
+#' @param ... Arguments to be passed to renv::restore()
+#'
+#' @return ???
+#' @export
+renv_restore <- function(dir, collection = "_posts", ...) {
+  renv::restore(
+    project = post_path(dir, collection),
+    lockfile = renv_lockfile(dir, collection),
+    ...
+  )
+}
+
