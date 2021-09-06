7d3d34b63093e18fead9c5815201e8b673f159e9
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-06 14:24:43 +1000

    various renv tweaks

--- R/renv.R
+++ R/renv.R
@@ -1,74 +1,5 @@
 
 
-#' Locate the renv project environment for a distill article
-#'
-#' @param dir The folder in which the article is located
-#' @param collection The collection the article belongs to (default = "_posts")
-#'
-#' @return Character string with the path
-#' @export
-#' @details By default, renv will either create the project library in the same
-#' folder as the post (if you're using renv::init), or else it will store the
-#' library in a temporary directory (if you're using renv::use with default
-#' parameters). The former causes problems with distill because the distill will
-#' recursively search the renv project library looking for posts, and it does
-#' sometimes find some! This is undesirable because it can break the blog.
-#' The latter is a very good default for users who just want to share a
-#' reproducible script (which was the motivation for renv::use) but it's
-#' inelegant if you wan to manage the R environments within your blog because
-#' the libraries aren't included within your blog. To address this, the
-#' refinery package places the renv.lock file in the post folder but moves the
-#' renv library into a separate top level "_renv" directory that distill won't
-#' look at when searching for posts
-renv_path <- function(dir, collection = "_posts") {
-  fs::path(
-    rprojroot::find_root("_site.yml"),
-    "_renv",
-    collection,
-    dir
-  )
-}
-
-#' Locate a distill article
-#'
-#' @param dir The folder in which the article is located
-#' @param collection The collection the article belongs to (default = "_posts")
-#'
-#' @return Character string with the path
-#' @export
-post_path <- function(dir, collection = "_posts") {
-  fs::path(
-    rprojroot::find_root("_site.yml"),
-    collection,
-    dir
-  )
-}
-
-#' Locate the renv lockfile associated with a distill article
-#'
-#' @param dir The folder in which the article is located
-#' @param collection The collection the article belongs to (default = "_posts")
-#'
-#' @return Character string with the path
-#' @export
-#' @details The renv.lock file is stored in the post folder
-renv_lockfile <- function(dir, collection = "_posts") {
-  fs::path(post_path(dir, collection), "renv.lock")
-}
-
-
-#' Locate the library for the post
-#'
-#' @param dir The folder in which the article is located
-#' @param collection The collection the article belongs to (default = "_posts")
-#'
-#' @return path
-#' @export
-renv_library <- function(dir, collection = "_posts") {
-  renv::paths$library(project = renv_path(dir, collection))
-}
-
-
 #' Load the renv library
 #'
 #' @param dir The folder in which the article is located
@@ -87,12 +18,12 @@ renv_load <- function(dir, collection = "_posts", ...) {
 #' @param dir The folder in which the article is located
 #' @param collection The collection the article belongs to (default = "_posts")
 #' @param ... Arguments to be passed to renv::snapshot()
-#' @param type Type of snapshot to take (defaults to "all")
+#' @param type Type of snapshot to take (defaults to "implicit")
 #' @param prompt Prompt user?
 #'
 #' @return ???
 #' @export
-renv_snapshot <- function(dir, collection = "_posts", type = "all", prompt = FALSE, ...) {
+renv_snapshot <- function(dir, collection = "_posts", type = "implicit", prompt = FALSE, ...) {
   renv::snapshot(
     project = post_path(dir, collection),
     lockfile = renv_lockfile(dir, collection),
@@ -101,6 +32,7 @@ renv_snapshot <- function(dir, collection = "_posts", type = "all", prompt = FAL
     ...
   )
 }
+
 
 #' Restore the renv project
 #'
@@ -156,13 +88,40 @@ renv_new <- function(dir, collection = "_posts") {
 #' @return ??
 #' @export
 renv_delete <- function(dir, collection = "_posts") {
-  delete_if_exists(renv_lockfile(dir, collection))
-  renv_dir <- renv_path(dir, collection)
-  fs::dir_delete(renv_dir)
+  renvdir <- renv_path(dir, collection)
+  lockfile <- renv_lockfile(dir, collection)
+  if(fs::file_exists(lockfile)) fs::file_delete(lockfile)
+  fs::dir_delete(renvdir)
 }
 
-delete_if_exists <- function(file) {
-  if(fs::file_exists(file)) fs::file_delete(file)
+
+# helpers -----------------------------------------------------------------
+
+
+renv_path <- function(dir, collection = "_posts") {
+  fs::path(
+    rprojroot::find_root("_site.yml"),
+    "_renv",
+    collection,
+    dir
+  )
+}
+
+post_path <- function(dir, collection = "_posts") {
+  fs::path(
+    rprojroot::find_root("_site.yml"),
+    collection,
+    dir
+  )
+}
+
+renv_lockfile <- function(dir, collection = "_posts") {
+  fs::path(post_path(dir, collection), "renv.lock")
+}
+
+
+renv_library <- function(dir, collection = "_posts") {
+  renv::paths$library(project = renv_path(dir, collection))
 }
 
 
