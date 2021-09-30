72cb783fb2057c3333aa41668bc9832f526f7f33
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-30 15:52:42 +1000

    adds initial sitrep function

--- R/lookup.R
+++ R/lookup.R
@@ -1,15 +1,47 @@
 
-#' Returns articles matching a pattern
+#' Locate articles matching a pattern
 #'
 #' @param pattern Regular expression
-#' @param collection Folder to look in
+#' @param collection The collection the article belongs to (default = "_posts")
 #'
 #' @return Character vector of paths
 #' @export
-articles_matching <- function(pattern, collection = "_posts") {
+article_locate <- function(pattern, collection = "_posts") {
   list.files(
     path = fs::path(site_root(), collection),
     pattern = pattern,
     include.dirs = TRUE
   )
+}
+
+
+#' Situation report for an article
+#'
+#' @param dir The folder in which the article is located
+#' @param collection The collection the article belongs to (default = "_posts")
+#'
+#' @details Provides a minimal "situation report" for a post, listing the
+#' folders associated with the post. Later versions will check renv status, I
+#' hope.
+#'
+#' @return Invisibly returns a list containing the paths
+#' @export
+#'
+article_sitrep <- function(dir, collection = "_posts") {
+
+  sitrep <- list(
+    site = site_root(),
+    post = post_path(dir, collection),
+    lockfile = renv_lockfile(dir, collection),
+    library = renv_library(dir, collection)
+  )
+
+  cli::cli({
+    cli::cli_text("site folder: ", sitrep$site)
+    cli::cli_text("post folder: ", sitrep$post)
+    cli::cli_text("lockfile:    ", sitrep$lockfile)
+    cli::cli_text("library:     ", sitrep$library)
+  })
+
+  return(invisible(sitrep))
 }
